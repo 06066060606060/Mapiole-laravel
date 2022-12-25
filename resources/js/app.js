fea00cfc243f0b8add4dc300003e3af6bd59f12a
06066060606060 <xmicky+git@hotmail.fr>
2022-12-25 12:55:36 +0400

    fix all slider

--- resources/js/app.js
+++ resources/js/app.js
@@ -28,6 +28,25 @@ barba.init();
                     },
                 },
             })
+
+            var swiper2 = new Swiper('.mySwiper', {
+                spaceBetween: 30,
+                centeredSlides: true,
+                autoplay: {
+                  delay: 4500,
+                  disableOnInteraction: false,
+                },
+                pagination: {
+                  el: '.swiper-pagination',
+                  clickable: true,
+                },
+                navigation: {
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                },
+              })
+
+
         })
 
         document.addEventListener('DOMContentLoaded', function() {
@@ -51,4 +70,21 @@ barba.init();
                     },
                 },
             })
+
+            var swiper2 = new Swiper('.mySwiper', {
+                spaceBetween: 30,
+                centeredSlides: true,
+                autoplay: {
+                  delay: 4500,
+                  disableOnInteraction: false,
+                },
+                pagination: {
+                  el: '.swiper-pagination',
+                  clickable: true,
+                },
+                navigation: {
+                  nextEl: '.swiper-button-next',
+                  prevEl: '.swiper-button-prev',
+                },
+              })
         })
--- resources/views/rent.blade.php
+++ resources/views/rent.blade.php
@@ -468,23 +468,5 @@
   </div>
 
      </div>
-       <script src='https://unpkg.com/swiper@6.8.4/swiper-bundle.min.js'></script>
-          <script>
-    var swiper = new Swiper('.mySwiper', {
-      spaceBetween: 30,
-      centeredSlides: true,
-      autoplay: {
-        delay: 4500,
-        disableOnInteraction: false,
-      },
-      pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-      },
-      navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-      },
-    });
-  </script>
+
  @endsection
