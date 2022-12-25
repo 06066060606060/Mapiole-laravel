d21b05b2b32a99c86aa4d009c97eb916f438fae2
06066060606060 <xmicky+git@hotmail.fr>
2022-12-25 12:48:47 +0400

    fix swipper

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,7 +11,6 @@
     </navbar>
     <main>
         @yield('main')
-            
     </main>
 <footer>
    @include('parts.footer')
@@ -20,57 +19,10 @@
 
     @vite('resources/js/app.js')
 
-    <script src="https://cdn.jsdelivr.net/npm/@barba/core"></script>
     <script src='https://unpkg.com/swiper@6.8.4/swiper-bundle.min.js'></script>
-       <script>
-        barba.init();
-        barba.hooks.afterEnter((data) => {
-            const swiper = new Swiper('.swiper-container', {
-                loop: true,
-                slidesPerView: 1.5,
-                spaceBetween: 24,
-                centeredSlides: true,
-                autoplay: {
-                    delay: 8000,
-                },
-                breakpoints: {
-                    640: {
-                        slidesPerView: 2,
-                    },
-                    800: {
-                        slidesPerView: 3,
-                    },
-                    1100: {
-                        slidesPerView: 4,
-                    },
-                },
-            })
-        });
 
-        document.addEventListener('DOMContentLoaded', function() {
-            const swiper = new Swiper('.swiper-container', {
-                loop: true,
-                slidesPerView: 1.5,
-                spaceBetween: 24,
-                centeredSlides: true,
-                autoplay: {
-                    delay: 8000,
-                },
-                breakpoints: {
-                    640: {
-                        slidesPerView: 2,
-                    },
-                    800: {
-                        slidesPerView: 3,
-                    },
-                    1100: {
-                        slidesPerView: 4,
-                    },
-                },
-            })
-        })
-
-          document.addEventListener("scroll", () => {
+ <script>
+  document.addEventListener("scroll", () => {
   var scroll_position = window.scrollY;
   if (scroll_position < 60) {
     navbar.style.backgroundColor = "transparent";
