2a1f7c97636d07b7e6db0ce5a4aa9c85c3f6259a
06066060606060 <xmicky+git@hotmail.fr>
2023-01-11 23:34:06 +0400

    up

--- resources/views/vendor/backpack/base/dashboard.blade.php
+++ resources/views/vendor/backpack/base/dashboard.blade.php
@@ -69,7 +69,7 @@
                 </div>
   @if (backpack_user()->role == 'admin')
             <p class="w-1/4 px-4 py-2 mx-8 text-xs text-gray-800 bg-white rounded shadow -pl-2">
-                Derniéres mise à jour: 2.8<br>
+                Derniéres mise à jour: 3<br>
                 -livechat<br>
                 -page poster annonce<br>
                 -page dynamique<br>
@@ -77,6 +77,8 @@
                 -ajout recherche<br>
                 -fonction search rent<br>
                 -fonction search buy<br>
+                -code login facebook<br>
+
             </p>
         @endif
             </div>
